025472463da158a7472e47bbee73b791df54d2b9
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-30 19:51:57 +0200

    update

---
+++ Pages/Categories/Mathematic.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FORUM_CZAT.Pages.Categories
+{
+    public class MathematicModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
