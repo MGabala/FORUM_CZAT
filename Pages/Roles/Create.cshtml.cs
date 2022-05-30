aaff23437b65767957aa44badade47c2c2e5cfa6
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-30 16:18:15 +0200

    ASP.NET Core Identity works

---
+++ Pages/Roles/Create.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FORUM_CZAT.Pages.Roles
+{
+    public class CreateModel : AdminPageModel
+    {
+        public RoleManager<IdentityRole> RoleManager;
+        public CreateModel(UserManager<IdentityUser> userManager,
+                RoleManager<IdentityRole> roleManager)
+        {
+            RoleManager = roleManager;
+        }
+        [BindProperty]
+        public string Name { get; set; } = string.Empty;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole { Name = Name };
+                IdentityResult result = await RoleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("List");
+                }
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return Page();
+        }
+    }
+}
