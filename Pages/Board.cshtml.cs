48eb2af41dbcb652d69462de522ecc6bd7813397
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-06-05 13:52:24 +0200

    update

--- Pages/Board.cshtml.cs
+++ Pages/Board.cshtml.cs
@@ -4,58 +4,20 @@ using Microsoft.Data.Sqlite;
 
 namespace FORUM_CZAT.Pages.Categories
 {
-    public class AstronomyModel : PageModel
+    public class BoardModel : PageModel
     {
         private IPostRepository _repository;
         private readonly string _connectionString = string.Empty;
         public IEnumerable<AfterApprovalPost> AfterApprovalPost { get; set; }
 
-        public AstronomyModel(IConfiguration configuration, IPostRepository repository)
+        public BoardModel(IConfiguration configuration, IPostRepository repository)
         {
             _connectionString = configuration["ConnectionStrings:DB"];
             _repository = repository;
         }
         public async Task OnGetAsync()
         {
-            AfterApprovalPost = await _repository.GetAllPostsAfterApprovalForAstronomyAsync();
-        }
-        public async Task<IActionResult> OnPostAsync(string title, string description,string category, string author)
-        {
-            if(category=="Select category")
-            {
-                return RedirectToPage("/Error");
-            }
-            else
-            {
-                _repository.AddPost(title, description, author, category, DateTime.Now);
-            }
-            //string category = "Astronomy";
-           
-    //        //var query = $"INSERT INTO [PostsBeforeApproval] (Title, Description, Author, Category, CreationTime) VALUES ('{title}','{description}','{author}','{category}','{DateTime.Now}')";
-
-    //        using (var con = new SqliteConnection(_connectionString))
-    //        using (var cmd = new SqliteCommand())
-    //        {
-    //            cmd.CommandText =
-    //@"
-    //INSERT INTO [PostsBeforeApproval] 
-    //(Title, Description, Author, Category, CreationTime) 
-    //VALUES 
-    //($title, $description, $author, $category, $creationtime)
-    //";
-    //            cmd.Parameters.AddWithValue("$title", title);
-    //            cmd.Parameters.AddWithValue("$description", description);
-    //            cmd.Parameters.AddWithValue("$author", author);
-    //            cmd.Parameters.AddWithValue("$category", category);
-    //            cmd.Parameters.AddWithValue("$creationtime", DateTime.Now.ToString());
-
-
-    //            cmd.Connection = con;
-    //            con.Open();
-    //           // cmd.CommandText = query;
-    //            cmd.ExecuteNonQuery();
-    //        }
-            return RedirectToPage("/AfterPostInformation");
+         
         }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FORUM_CZAT.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FORUM_CZAT.Pages
@@ -6,15 +7,20 @@ namespace FORUM_CZAT.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private IPostRepository _repository;
+        private readonly string _connectionString = string.Empty;
+        public IEnumerable<AfterApprovalPost> AfterApprovalPost { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration, IPostRepository repository)
         {
             _logger = logger;
+            _connectionString = configuration["ConnectionStrings:DB"];
+            _repository = repository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnPostAsync(string title, string description, string category, string author)
         {
-
+            _repository.AddPost(title, description, author, category, DateTime.Now);
+            return RedirectToPage("/AfterPostInformation");
         }
     }
 }
